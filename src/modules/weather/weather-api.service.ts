2cfb7c76a1c4dcbee067aaf006554515f189eb5f
Cyberoiide <clement.bosle.dev@gmail.com>
2026-07-14 15:52:18 +0200

    feat: add timeout

--- src/modules/weather/weather-api.service.ts
+++ src/modules/weather/weather-api.service.ts
@@ -7,7 +7,7 @@ export class WeatherAPIService {
   async getWeather(query: string) {
     const weatherAPIKey = this.configService.get('WEATHER_API_KEY');
     const url = `https://api.weatherapi.com/v1/forecast.json?key=${weatherAPIKey}&q=${query}&days=3&aqi=yes&alerts=yes`;
-    const response: Response = await fetch(url);
+    const response: Response = await fetch(url, { signal: AbortSignal.timeout(5000) });
 
     if (response.status !== 200) {
       throw new HttpException(
