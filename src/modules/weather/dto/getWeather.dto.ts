aaf38ff3c96f87d0ffdf6fbe60c01d3e067842dc
Cyberoiide <clement.bosle.dev@gmail.com>
2026-07-14 12:31:42 +0200

    feat: update dto weather with range

--- src/modules/weather/dto/getWeather.dto.ts
+++ src/modules/weather/dto/getWeather.dto.ts
@@ -2,8 +2,8 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 export const Coordinates = z.object({
-  lat: z.coerce.number(),
-  long: z.coerce.number(),
+  lat: z.coerce.number().min(-90).max(90),
+  long: z.coerce.number().min(-180).max(180),
 });
 
 export class GetWeatherDTO extends createZodDto(Coordinates) {}
